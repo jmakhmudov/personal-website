608b946f420e8923d541593e56321fc3c3ca63de
JALOLIDDIN MAKHMUDOV <makhmudov@tuta.io>
2023-04-05 15:36:11 +0500

    fetching data

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import '@/styles/globals.css'
 import '@/styles/Components.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
+import { supabase } from '@/components/supabase'
 
 function MyApp({ Component, pageProps }) {
-  const [supabase] = useState(() => createBrowserSupabaseClient())
+  
 
   return (
     <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
